e6ca34e3e123fe4cdd60ae83389c64bd76581b8e
Theanh9323 <141454274+Theanh9323@users.noreply.github.com>
2024-04-08 13:29:01 +0700

    initial

---
+++ SIMSS/SIMSS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SIMSS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
